afc46b031b90cb5646d6c74e9d6274544947b866
Mohamed Ali Dia <alidia378@gmail.com>
2025-07-14 20:25:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 import seaborn as sns
 import seaborn.objects as so
 import spacy
-import subprocess
 from spacy.lang.fr.stop_words import STOP_WORDS
 from io import BytesIO
 import joblib
@@ -33,11 +32,7 @@ def load_spacy():
     return spacy.load("fr_core_news_sm")
 
 # Charger ou installer fr_core_news_sm
-try:
-    nlp = spacy.load("fr_core_news_sm")
-except OSError:
-    subprocess.run(["python", "-m", "spacy", "download", "fr_core_news_sm"])
-    nlp = spacy.load("fr_core_news_sm")
+nlp = spacy.load("fr_core_news_sm")
 
 nlp = load_spacy()
 
