488048b2c1f2008ab8d79e5cd0accd21f9438c8a
Mohamed Ali Dia <alidia378@gmail.com>
2025-07-15 08:47:16 +0000

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installation des dépendances..."
-pip install -r requirements.txt
-echo "Téléchargement du modèle linguistique français..."
-python -m spacy download fr_core_news_sm
-echo "✅ Installation terminée."
